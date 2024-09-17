e5c7f1e6c0d30be42d5f46f963edcdfc9857d597
mmio0004 <113961334+mmio0004@users.noreply.github.com>
2024-09-17 11:05:04 +0800

    add homework code and files

---
+++ hypertension_vis.js
@@ -0,0 +1,7 @@
+var vg_1 = "hypertension_map.vg.json";
+
+vegaEmbed("#map", vg_1).then(function(result) {
+    
+    //Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
